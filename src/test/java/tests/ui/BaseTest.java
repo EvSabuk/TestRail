be389c024afa138986e177729528386e172f5a7a
e.sabuk <sabuk.zhenya@gmail.com>
2025-07-13 23:39:20 +0400

    fix case

--- src/test/java/tests/ui/BaseTest.java
+++ src/test/java/tests/ui/BaseTest.java
@@ -13,11 +13,9 @@ import pages.*;
 import listeners.TestListener;
 import steps.api.DeleteProjectStep;
 import steps.ui.*;
-
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-
 import static listeners.AllureUtils.takeScreenshot;
 
 @Listeners(TestListener.class)
